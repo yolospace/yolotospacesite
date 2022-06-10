7f464f2b45b16835bb0ca32ec4ec70dcbdd4918f
00MB <s2061204@ed.ac.uk>
2022-06-10 21:31:07 +0100

    feat: added moralis

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import { AppProps } from 'next/app';
+import { MoralisProvider } from 'react-moralis';
 
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const moralis_app_id = process.env.NEXT_PUBLIC_MORALIS_APP_ID ?? '';
+  const moralis_server = process.env.NEXT_PUBLIC_MORALIS_SERVER ?? '';
+
+  return (
+    <MoralisProvider serverUrl={moralis_server} appId={moralis_app_id}>
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+import { useMoralis } from 'react-moralis';
 
 import Seo from '@/components/Seo';
 
 export default function HomePage() {
+  const { authenticate, isAuthenticated } = useMoralis();
+  useEffect(() => {
+    if (isAuthenticated) {
+      // add your logic here
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated]);
+
+  const login = async () => {
+    if (!isAuthenticated) {
+      await authenticate({ signingMessage: 'Log in using Moralis' });
+    }
+  };
+
   return (
     <div>
       <Seo title='Home' />
-      <h1>Hello Next.js</h1>
+      <button onClick={login}>Moralis Metamask Login</button>
+      <h1 className='text-red-300'>Hello Next.js</h1>
     </div>
   );
 }
